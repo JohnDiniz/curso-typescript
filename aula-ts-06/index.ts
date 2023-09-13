55fe1260d5eb52783092cce12eb78079167c24ff
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-09-13 11:26:07 -0300

    add: aula 06

--- aula-ts-06/index.ts
+++ aula-ts-06/index.ts
@@ -26,3 +26,36 @@ function play(game) {
 }
 
 play(game);
+
+// interface Game {
+//   id: number;
+//   platform: Platform;
+//   title: string;
+//   publisher: string;
+//   launch?: string | Date;
+// }
+
+// interface Platform {
+//   id: number;
+//   name: string;
+// }
+
+// const game: Game = {
+//   id: 1,
+//   platform: {
+//     id: 1,
+//     name: "Playstation",
+//   },
+//   title: "The Last of Us",
+//   publisher: "Naughty Dog",
+//   launch: "2013-06-14", // pode ser um Date. É opcional (para jogos ainda não lançados).
+// };
+
+// const games: Game[] = [game];
+
+// function play(game) {
+//   // runs the game
+//   console.log(game);
+// }
+
+// play(game);
